5da01a8ad59cb5e2cb60ed8e9d3317ad12933a3e
Vaibhav Manohar Davande <84057897+vaibhavmde@users.noreply.github.com>
2022-04-02 13:33:46 +0530

    added comments

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT||5000;
-const fs=require('fs');
+const express = require('express'); //import express module
+const app = express();                  //created a app of express module
+const PORT = process.env.PORT||5000;      //port to default system port number or 5000
+const fs=require('fs');                    //import fs module
 
-if(!fs.existsSync('./watch')){
-  fs.mkdir('./watch',(err)=>{
-    if(err) throw err;
+if(!fs.existsSync('./watch')){             //checking the folder is already present the directory
+  fs.mkdir('./watch',(err)=>{                //if not make one
+    if(err) throw err;                                 
     console.log('folder created');
   });
 }
-setInterval(()=>{
+                    
+//to append the time in file  after every sec
+setInterval(()=>{                               
   fs.appendFile('./watch/CurrentTimeStamp.txt',`\n${new Date().toLocaleTimeString()}`,'utf8',(err)=>{
     if(err) throw err;
     console.log("Data stored");
   })
 },1000)
 
+//on get displaying the content of the file
 app.get('/',(req,res)=>{
   res.sendFile('./watch/CurrentTimeStamp.txt',{root:__dirname});
 })
 
+//listen to port
 app.listen(PORT,(err)=>{
   if(err) throw err;
   console.log("Listening to PORT",PORT);
